78451cf2f1b0b1f50cdeae861a6e8efea129b453
shinycorolla <shinycorolla@gmail.com>
2018-11-26 09:56:15 +0900

    hou~!

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'hidhih222ihi' });
+  res.render('index', { title: 'hid3hi33h222ihi' });
 });
 
 module.exports = router;
